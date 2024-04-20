3fdb6f78ef2e53d5371c6b38c01df31848fbf62c
unknown <gangforcoderepo@gmail.com>
2024-04-20 22:47:13 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {
+    setInput(input + e.target.value);
+  };
+
+  const calculate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  };
+
+  const clear = () => {
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="header">
+      <a href="https://gangforcode.com/" target="_blank" rel="noreferrer noopener" class="button-style">
+          Back to Tutorial
         </a>
-      </header>
-    </div>
+      </div>
+      <div className="calculator">
+        <div className="display">{input}</div>
+        <div className="keypad">
+          <button onClick={clear}>Clear</button>
+          <button onClick={handleInput} value="/">/</button>
+          <button onClick={handleInput} value="*">*</button>
+          <button onClick={handleInput} value="-">-</button>
+          <button onClick={handleInput} value="7">7</button>
+          <button onClick={handleInput} value="8">8</button>
+          <button onClick={handleInput} value="9">9</button>
+          <button onClick={handleInput} value="+">+</button>
+          <button onClick={handleInput} value="4">4</button>
+          <button onClick={handleInput} value="5">5</button>
+          <button onClick={handleInput} value="6">6</button>
+          <button onClick={handleInput} value="1">1</button>
+          <button onClick={handleInput} value="2">2</button>
+          <button onClick={handleInput} value="3">3</button>
+          <button onClick={handleInput} value="0">0</button>
+          <button onClick={handleInput} value=".">.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </>
   );
 }
 
